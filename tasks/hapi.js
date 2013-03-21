a9c4fd6afc279ca336137c73de618f28a87d5bcd
Aurélien Thieriot <a.thieriot@gmail.com>
2013-03-21 20:12:36 +0000

    Launch the server in a child process
    Allows relaunch of the task via grunt-contrib-watch

--- tasks/hapi.js
+++ tasks/hapi.js
@@ -1,53 +1,40 @@
 
 'use strict';
 
-var cleanPath = function(string) {
-  var separator = '/';
-
-  if (string.charAt(string.length - 1) !== separator) {
-    return string + separator;
-  }
-   
-  return string;
-};
-
 module.exports = function(grunt) {
+
+  var running;
    
   grunt.registerMultiTask('hapi', 'Start an Hapi web server.', function() {
+    var done = this.async();
 
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      //debug: false,
       server: null,
-      // filepath that points to a module that exports an Hapi server object
       bases: {'/': '.'}
     });
 
-		options.debug = grunt.option('debug') || options.debug;
-
-    if (options.server) {
-      try {
-        var http = require(options.server);
-
-        if (options.bases) {
-          for (var key in options.bases) {
-            http.route({
-              method: 'GET', path: cleanPath(key) + '{path*}',
-              handler: {
-                directory: {
-                  path: options.bases[key], listing: true
-                }
-              }
-            });
-          }
-        }
- 
-        http.start();
-      } catch (e) {
-        grunt.fatal('Hapi ["' + options.server + '"] -  ' + e);
-      }
-    } else {
-      grunt.fatal('Hapi should provide an Hapi server instance.');
+    if (running !== undefined) {
+      running.disconnect();
     }
+  
+    // Starting a child process to launch the server in
+    running = require('child_process').fork(__dirname + '/../lib/forkme');
+    running.send(options);
+
+    // Handle errors or success happening in the child process
+    running.on('message', function(error) {
+      if (error) {
+        grunt.fatal(error);
+      }
+
+      done();
+    });
+
+    process.on('exit', function() {
+      if (running !== undefined) {
+        running.disconnect();
+      }
+    });
   });
 };
